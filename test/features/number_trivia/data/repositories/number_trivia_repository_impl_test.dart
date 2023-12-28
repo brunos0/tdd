0aea1cdd85805161873853c72bdbc40beed0a481
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2023-12-28 16:22:52 -0300

    create data repositories mocks

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -4,7 +4,9 @@ import 'package:mockito/mockito.dart';
 import 'package:resocoder/core/platform/network_info.dart';
 import 'package:resocoder/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:resocoder/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:resocoder/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
+import '../../domain/usecases/get_concrete_number_trivia_test.mocks.dart';
 @GenerateNiceMocks([
   MockSpec<NumberTriviaRemoteDataSource>(),
   MockSpec<NumberTriviaLocalDataSource>(),
@@ -12,14 +14,19 @@ import 'package:resocoder/features/number_trivia/data/datasources/number_trivia_
 ])
 import 'number_trivia_repository_impl_test.mocks.dart';
 
-/*
-@GenerateNiceMocks([MockSpec<NumberTriviaLocalDataSource>()])
-import 'number_trivia_repository_impl_test.mocks.dart';
-
-@GenerateNiceMocks([MockSpec<NetworkInfo>()])
-import 'number_trivia_repository_impl_test.mocks.dart';
-*/
 void main() {
+  late NumberTriviaRepository numberTriviaRepository;
+  late NumberTriviaRemoteDataSource mockRemoteDataSource;
+  late NumberTriviaLocalDataSource mockLocalDataSource;
+  late NetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    numberTriviaRepository = MockNumberTriviaRepository();
+    mockRemoteDataSource = MockNumberTriviaRemoteDataSource();
+    mockLocalDataSource = MockNumberTriviaLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+  });
+
   testWidgets('number trivia repository impl ...', (tester) async {
     // TODO: Implement test
   });
