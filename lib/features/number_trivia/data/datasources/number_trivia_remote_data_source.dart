9819ee9867d0d48ebaa985351210280ca749497e
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-04 20:17:28 -0300

    local data source test

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,20 +1,38 @@
 //import 'package:dartz/dartz.dart';
 //import 'package:resocoder/core/error/failures.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:resocoder/core/error/exceptions.dart';
+import 'package:resocoder/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:resocoder/features/number_trivia/domain/entities/number_trivia.dart';
 
+import 'package:http/http.dart';
+
 abstract class NumberTriviaRemoteDataSource {
-  Future<NumberTrivia> getConcreteNumberTrivia(int number);
-  Future<NumberTrivia> getRandomNumberTrivia();
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
+  Future<NumberTriviaModel> getRandomNumberTrivia();
 }
 
 class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
-  @override
-  Future<NumberTrivia> getConcreteNumberTrivia(int number) {
-    throw UnimplementedError();
-  }
+  const NumberTriviaRemoteDataSourceImpl({required this.client});
+  final Client client;
 
   @override
-  Future<NumberTrivia> getRandomNumberTrivia() {
-    throw UnimplementedError();
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl(number.toString());
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(Uri.parse('http://numbersapi.com/$url'),
+        headers: {'Content-Type:': 'application/json'});
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
   }
 }
--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -6,5 +6,5 @@ class NumberTrivia extends Equatable {
   const NumberTrivia({required this.text, required this.number});
 
   @override
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [text, number];
 }
