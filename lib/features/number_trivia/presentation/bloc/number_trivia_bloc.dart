b6b4ced5436e2d8a42fc87c61767640184ced697
Bruno Horvat <brunohorvat@Mac-MCC-C02G.local>
2024-01-06 14:02:55 -0300

    implements bloc part1.1

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -6,15 +6,27 @@ import 'package:resocoder/features/number_trivia/domain/usecases/get_random_numb
 import 'package:resocoder/features/number_trivia/presentation/bloc/number_trivia_event.dart';
 import 'package:resocoder/features/number_trivia/presentation/bloc/number_trivia_state.dart';
 
+const String serverFailureMessage = 'Server Failure';
+const String cacheFailureMessage = 'Cache Failure';
+const String invalidInputFailureMessage =
+    'Invalid Input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   NumberTriviaBloc({
     required this.getConcreteNumberTrivia,
     required this.getRandomNumbertrivia,
     required this.inputConverter,
   }) : super(Empty()) {
-    on<NumberTriviaEvent>((event, emit) {
+    on<NumberTriviaEvent>(
+        (NumberTriviaEvent event, Emitter<NumberTriviaState> emit) async {
       if (event is GetTriviaForConcreteNumber) {
-        inputConverter.stringToUnsignedInteger(event.numberString);
+        final inputEither =
+            inputConverter.stringToUnsignedInteger(event.numberString);
+        //emit(Error(message: invalidInputFailureMessage));
+
+        inputEither.fold((failure) async {
+          emit(Error(message: invalidInputFailureMessage));
+        }, (success) {});
       }
     });
   }
